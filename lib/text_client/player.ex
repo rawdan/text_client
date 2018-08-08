5016a3d90b4ae381f7bbf670360c9f18b42d50b6
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-08-08 12:59:54 +0300

    refactor: extract duplication in private method

--- lib/text_client/player.ex
+++ lib/text_client/player.ex
@@ -4,13 +4,11 @@ defmodule TextClient.Player do
 
   # won, lost, good guess, bad guess, already used, initializing
   def play(%State{tally: %{ game_state: :won}}) do
-    IO.puts "You've won!"
-    exit(:normal)
+    exit_with_message("You've won!")
   end
 
   def play(%State{tally: %{ game_state: :lost}}) do
-    IO.puts "Sorry, you've lost!"
-    exit(:normal)
+    exit_with_message("Sorry, you've lost!")
   end
 
   defp exit_with_message(msg) do
